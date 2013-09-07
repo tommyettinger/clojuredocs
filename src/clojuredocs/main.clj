0d1e847431f0467522793d5140ef8ee3c85e0468
Steven Degutis <steven@cleancoders.com>
2013-09-07 14:00:43 -0500

    Bare-bones doc-index

--- src/clojuredocs/main.clj
+++ src/clojuredocs/main.clj
@@ -8,6 +8,9 @@
 
 (def style
   (css
+   [:#home
+    [:a {:text-decoration "none"
+         :margin "5px"}]]
    [:#container {:margin "30px"}]
    [:#primary {:font-weight "bold"}]
    [:ul {:list-style-type "none"
@@ -38,87 +41,70 @@
     (= nm "_dotdot") ".."
     :else nm))
 
-(defn show-name [nm old-names]
-  (let [name-meta (meta nm)
-        fq-enqued-name (str (ns-name (:ns name-meta)) "/" (enque (:name name-meta)))
-        fq-dequed-name (str (ns-name (:ns name-meta)) "/" (:name name-meta))
-        body (html
-              (html5
-               [:head [:style {:type "text/css"} style]]
-               [:body
-                [:div#container
-                 [:ul#primary
-                  (if (empty? (:arglists name-meta))
-                    [:li (:name name-meta)]
-                    (for [args (:arglists name-meta)]
-                      [:li "(" (interpose " " (cons (:name name-meta) args)) ")"]))]
-                 [:pre "  " (:doc name-meta)]
+(defn template [& body]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (html
+          (html5
+           [:head [:style {:type "text/css"} style]]
+           [:body
+            [:div#container
+             body
+             [:footer
+              [:hr {:align "left"}]
+              [:p "Idea by "
+               [:a {:href "https://github.com/TimMc"} "TimMc"]
+               ". Webified by "
+               [:a {:href "https://github.com/sdegutis"} "sdegutis"]
+               ". Source "
+               [:a {:href "https://github.com/sdegutis/clojuredocs"} "on github"]
+               "."]]]]))})
 
-                 [:footer
-                  [:p
-                   [:a {:href "/"} "random"]
-                   ", "
-                   [:a {:href (str "/" (enque (:name name-meta)))}
-                    "permalink"]
-                   ", "
-                   [:a {:href (str "http://clojuredocs.org/clojure_core/" fq-enqued-name)}
-                    "clojuredocs"]
-                   ", "
-                   [:a {:href (str "http://clojure.github.io/clojure/clojure.core-api.html#" fq-dequed-name)}
-                    "official docs"]]
-                  (if-not (empty? old-names)
-                    [:p
-                     "Recent: "
-                     (interpose ", "
-                                (for [[idx nm] (map list (iterate inc 0) (take 3 old-names))]
-                                  [:a {:href (str "/recent/" idx)} nm]))])
-                  [:hr {:align "left"}]
-                  [:p "Idea by "
-                   [:a {:href "https://github.com/TimMc"} "TimMc"]
-                   ". Webified by "
-                   [:a {:href "https://github.com/sdegutis"} "sdegutis"]
-                   ". Source "
-                   [:a {:href "https://github.com/sdegutis/clojuredocs"} "on github"]
-                   "."]]]]))]
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body body}))
-
-(defn shuffled-names []
+(defn all-core-names []
   (->> clojure.core
        (quote)
        (the-ns)
        (ns-publics)
        (map first)
-       (shuffle)))
+       (map str)))
 
 (defn var-for [nm]
   (find-var (symbol "clojure.core" (str nm))))
 
-(defn get-stored-names [names-str]
-  (if names-str
-    (let [names (read-string names-str)]
-      (if-not (empty? names)
-        names))))
-
 (defroutes app
   (GET "/" [:as req]
-    (let [names (or (get-stored-names (:names (:session req)))
-                    (shuffled-names))
-          [nm & remainder] names
-          old-names (cons nm (get-stored-names (:old-names (:session req))))]
-      (assoc (show-name (var-for nm) old-names)
-        :session {:names (pr-str remainder)
-                  :old-names (pr-str (take 20 old-names))})))
+    (let [names (all-core-names)
+          groups (group-by #(.charAt % 0) names)]
+      (template
+       [:div#home
+        (for [[group-char names] groups]
+          [:p
+           [:h4 (format "%c" group-char)]
+           (interpose " "
+                      (for [nm names]
+                        [:a {:href (str "/" (deque (str nm)))}
+                         nm]))])])))
   (GET "/:nm" [nm :as req]
-    (let [old-names (get-stored-names (:old-names (:session req)))]
-      (show-name (var-for (deque nm)) old-names)))
-  (GET "/recent/:n" [n :as req]
-    (let [old-names (get-stored-names (:old-names (:session req)))
-          n (Integer/parseInt n)]
-      (if (< -1 n (count old-names))
-        (if-let [nm (nth old-names n)]
-          (show-name (var-for nm) old-names))))))
+    (let [nm (var-for (deque nm))
+          name-meta (meta nm)
+          fq-enqued-name (str (ns-name (:ns name-meta)) "/" (enque (:name name-meta)))
+          fq-dequed-name (str (ns-name (:ns name-meta)) "/" (:name name-meta))]
+      (template
+       [:ul#primary
+        (if (empty? (:arglists name-meta))
+          [:li (:name name-meta)]
+          (for [args (:arglists name-meta)]
+            [:li "(" (interpose " " (cons (:name name-meta) args)) ")"]))]
+       [:pre "  " (:doc name-meta)]
+       [:p
+        [:a {:href (str "/" (enque (:name name-meta)))}
+         "permalink"]
+        ", "
+        [:a {:href (str "http://clojuredocs.org/clojure_core/" fq-enqued-name)}
+         "clojuredocs"]
+        ", "
+        [:a {:href (str "http://clojure.github.io/clojure/clojure.core-api.html#" fq-dequed-name)}
+         "official docs"]]))))
 
 (defn -main [port]
   (jetty/run-jetty (-> app
